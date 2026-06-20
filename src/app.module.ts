02a12df63da0c5cc2569efe7c9c643f9ed88705c
Jaime Pineda <jpin730@pm.me>
2026-06-19 21:03:29 -0600

    feat: :sparkles: add schedule module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
+import { ScheduleModule } from '@nestjs/schedule'
 
 import { ConfigModule } from '@config/config.module'
 import { DatabaseModule } from '@database/database.module'
@@ -13,6 +14,7 @@ import { httpLogger } from '@common/middlewares/http-logger.middleware'
   imports: [
     ConfigModule,
     DatabaseModule,
+    ScheduleModule.forRoot(),
 
     // Features
     HealthModule,
